436940f6e99d89f14f26e7e4e4fd697118f5dbaf
Julius Rudolph <julius.rudolph@student.hpi.de>
2017-08-01 12:09:37 +0000

    added echo line for results at end of training

--- s5/local/run_am.sh
+++ s5/local/run_am.sh
@@ -160,5 +160,6 @@ for x in exp/*/decode*; do [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh; \
 done | sort -n -r -k2 > RESULTS.$USER.$time # to make sure you keep the results timed and owned
 
 echo training succedded
+echo "WER-RESULTS"
 cat RESULTS.$USER.$time
 exit 0
