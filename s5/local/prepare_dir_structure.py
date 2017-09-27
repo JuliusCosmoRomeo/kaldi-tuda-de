53cbd3696007edef6aebbe6807ee1847b3db4da1
Julius Rudolph <julius.rudolph@student.hpi.de>
2017-09-27 12:13:37 +0000

    added errno

--- s5/local/prepare_dir_structure.py
+++ s5/local/prepare_dir_structure.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 from common_utils import make_sure_path_exists
-import os
+import os, errno
 import sys
 
 def symlink_file(file1,file2):
@@ -40,7 +40,7 @@ make_sure_path_exists('data/local/dict/')
 
 #if exp and mfcc don't exist locally, create them as link to some other directory on a larger disk
 if not os.path.exists('exp/') and not os.path.exists('mfcc/'):
-    default_dir = '/models/' + sys.argv[1] + '/s5'
+    default_dir = '/models/' + sys.argv[1] 
     data_dir = default_dir
 
 
