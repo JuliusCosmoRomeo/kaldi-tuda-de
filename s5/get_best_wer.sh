cbf36de6034713f99c84456f85e04c598ad90e33
Julius Rudolph <julius.rudolph@student.hpi.de>
2017-10-09 12:38:58 +0000

    fixed get_best_wer

--- s5/get_best_wer.sh
+++ s5/get_best_wer.sh
@@ -1,2 +1,5 @@
 time=$(date +"%Y-%m-%d-%H-%M-%S")
-for x in $1/exp/*/$1; do [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh; done | sort -n -r -k2 > $1/DEC_RESULTS.$USER.$time
+for x in $1/exp/sgmm_5a/$1 $1/exp/tri3b/$1; do [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh; done | sort -n -r -k2 > $1/DEC_RESULTS.$USER.$time
+cat $1/DEC_RESULTS.$USER.$time
+
+
